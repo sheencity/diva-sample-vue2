99587b395b5fd69e60463baa01e715182e53594b
密西西比何 <48949824+DoubleStorm@users.noreply.github.com>
2021-08-24 18:14:00 +0800

    fix: 创建diva对象的方式

--- src/services/diva.service.js
+++ src/services/diva.service.js
@@ -1,6 +1,9 @@
 import {
   Diva
 } from '@sheencity/diva-sdk';
+import { 
+  CefAdapter
+} from '@sheencity/diva-sdk-adapter';
 export class DivaService {
   // divaClient
   client;
@@ -17,11 +20,8 @@ export class DivaService {
     const uri = 'http://127.0.0.1:3000';
     const apiKey = '<replace_your_api_key_here>';
     console.log(uri, apiKey, container);
-    const diva = new Diva({
-      container,
-      apiKey,
-      uri
-    });
+    const adapter = new CefAdapter(container);
+    const diva = new Diva({ apiKey, adapter });
     console.log('diva is', diva);
     this.client = await diva.init();
     console.log('client is', this.client);
