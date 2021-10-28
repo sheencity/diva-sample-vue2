c22a950a86cecad860904f83c827c50c6265a665
何沁 <heqin@sheencity.com>
2021-10-28 11:36:58 +0800

    chore: 使用 0.3.0 版本 sdk

--- src/views/Monitor.vue
+++ src/views/Monitor.vue
@@ -81,8 +81,15 @@
           monitor = await this.getModelByName(monitor);
         }
         if (!url) return;
-        await monitor.setWebWidget(new URL(url), 500, 280);
-        data.changeCode(`model.setWebWidget(new URL('${url}'), 500, 280)`);
+        await monitor.setWebWidget(new URL(url), {
+          width: 500,
+          height: 280,
+          mouseInput: true,
+          keyboardInput: true,
+        });
+        data.changeCode(
+          `model.setWebWidget(new URL('${url}'), { width: 500, height: 280, mouseInput: true, keyboardInput: true })`
+        );
       },
       async refresh(monitorEqui) {
         try {
